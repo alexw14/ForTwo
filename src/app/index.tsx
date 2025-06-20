b2b90cdf90aac79a5f0809bf70fbc689591ac104
Alex Wong <alexw14@gmail.com>
2025-06-20 12:40:19 -0700

    add bottom tabs navigation

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
